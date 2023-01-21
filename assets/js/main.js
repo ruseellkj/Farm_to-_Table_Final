5edd205e5fea9426ec0aa8ff0a03729400670600
Rushil Jariwala <rkjjariwala@gmail.com>
2023-01-21 19:42:34 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,4 @@
-/**
-* Template Name: Restaurantly - v3.10.0
-* Template URL: https://bootstrapmade.com/restaurantly-restaurant-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+
 (function() {
   "use strict";
 
